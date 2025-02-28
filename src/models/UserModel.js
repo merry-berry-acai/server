6ae782cb660d4bffbb75fc20f81c5a1aa28f86a3
finneh4249 <ethancornwill@outlook.com>
2025-02-28 17:26:04 +1100

    enhance User model with email validation and role enumeration

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,19 +1,35 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     uid: { type: String, required: true, unique: true }, // Firebase UID
     displayName: { type: String, required: true },
     email: {
-        type: String,
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [
+        /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+        "Please provide a valid email address",
+      ],
     },
     photoURL: { type: String },
-    favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
-    orderHistory: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Order' }],
-    role: { type: String, default: 'user' },
-}, {
-    timestamps: true
-});
 
-const User = mongoose.model('User', userSchema);
+    favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
+    orderHistory: [{ type: mongoose.Schema.Types.ObjectId, ref: "Order" }],
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = { User };
