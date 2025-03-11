93770ef3080cb095564dbcb7d9e656f193002ddc
danilo90lan <15630@coderacademy.edu.au>
2025-03-11 18:07:19 +1100

    sentry correctly initialized in server.js

--- src/server.js
+++ src/server.js
@@ -6,22 +6,13 @@ const cors = require("cors");
 const Sentry = require("@sentry/node");
 
 Sentry.init({
-  dsn: "https://sentry.io/api/projects/your-project-id/minidump/?sentry_key=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", // Replace with your actual DSN
-  integrations: [
-    // enable HTTP calls tracing
-    new Sentry.Integrations.Http({ tracing: true }),
-    // enable Express.js middleware tracing
-    new Sentry.Integrations.Express(),
-    ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
-  ],
-  // Performance Monitoring
-  tracesSampleRate: 1.0, // Capture 100% of transactions for performance monitoring
-  // Set environment, you can read this from process.env.NODE_ENV if you want
-  environment: "production", 
+  dsn: "https://2be293c706cb92cbb9e6484727c4b2a9@o4508947030867968.ingest.de.sentry.io/4508948514406480",
 });
 
-// The request handler must be the first middleware on the app
-app.use(Sentry.Handlers.request());
+console.log("Sentry initialized."); // Add this line
+
+Sentry.setupExpressErrorHandler(app);
+
 app.use(express.json());
 app.use(requestLogger);
 
@@ -40,15 +31,12 @@ app.get("/", (req, res) => {
 // Routes
 app.use("/items", require("./routes/ItemRoutes"));
 app.use("/orders", require("./routes/OrderRoutes"));
-app.use("/reviews", require("./routes/ReviewRoutes"));
 app.use("/users", require("./routes/UserRoutes"));
 app.use("/toppings", require("./routes/ToppingRoutes"));
 app.use("/categories", require("./routes/CategoryRoutes"));
 app.use("/checkout", require("./routes/Payment"));
 app.use("/images", require("./routes/ImageRoutes")); // Add the images route
 
-// The error handler must be before any other error middleware and after all controllers
-app.use(Sentry.Handlers.errorHandler());
 app.use(errorHandler);
 
 // Handle 404 - Route not found
